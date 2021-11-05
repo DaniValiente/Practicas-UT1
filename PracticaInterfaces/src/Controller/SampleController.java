1014c4a40c63b7ff03c0d6555f3a991b1659a61e
Daniel Valiente Muñoz <daniel.ma42585@cesurformacion.com>
2021-11-05 13:36:39 +0100

    Actualizacion v1.10

--- PracticaInterfaces/src/Controller/SampleController.java
+++ PracticaInterfaces/src/Controller/SampleController.java
@@ -1,12 +1,14 @@
 package Controller;
-import java.util.regex.Pattern;
+
+
+import javax.swing.JFormattedTextField;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
+
 
 
 public class SampleController  {
@@ -14,18 +16,9 @@ public class SampleController  {
 	@FXML
 	private  Button clean;
 	@FXML
-	private  TextField nombre, apellidos,direccion,ciudad,codigopostal,numerodetelefono;
+	private  TextField  nombre,apellidos,direccion,ciudad,codigopostal,numerodetelefono;
 	@FXML
 	private  DatePicker fechadenacimiento;
-	
-	@FXML 
-	private void eventKey(KeyEvent ev) {
-		Object evt =ev.getSource();
-		if(codigopostal.getText().length()>5) {
-			ev.consume();
-		}
-	}
-	
 	
 	
 	
@@ -40,6 +33,10 @@ public class SampleController  {
 	fechadenacimiento.getEditor().clear();
 	
 	}
+	
+	
+	
+	
 	
 }
      
