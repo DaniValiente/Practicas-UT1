200bf2f60f98efb975755a6cb12defad9f459117
Daniel Valiente Muñoz <daniel.ma42585@cesurformacion.com>
2021-11-05 09:23:13 +0100

    Actualizacion trabajo 05/11/2021

--- PracticaInterfaces/src/Controller/SampleController.java
+++ PracticaInterfaces/src/Controller/SampleController.java
@@ -1,13 +1,52 @@
 package Controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
-
-public class SampleController {
+public class SampleController  {
+	
+	@FXML
+	private  Button editar;
+	@FXML
+	private  TextField nombre;
+	@FXML
+	private  TextField apellidos;
+	@FXML
+	private  TextField direccion;
+	@FXML
+	private  TextField ciudad;
+	@FXML
+	private  TextField codigopostal;
+	@FXML
+	private  DatePicker fechadenacimiento;
+	@FXML
+	private  TextField numerodetelefono;
+	
+	public void editar(ActionEvent event) { 
+	nombre.setText("");
+	apellidos.setText("");
+	direccion.setText("");
+	ciudad.setText("");
+	codigopostal.setText("");
+	numerodetelefono.setText("");
 	
 	
-
 	}
+
+	
+	}
+	
+
+	
+	
+
 
 	
 	
